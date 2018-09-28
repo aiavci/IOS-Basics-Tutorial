6e7c31e2b03d647df910dbceffa0cc093d008e16
Ali Avci <aiavci@yahoo.com>
2018-09-27 21:29:40 -0400

    First commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Ali Avci on 2018-09-27.
+//  Copyright © 2018 Ali Avci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
